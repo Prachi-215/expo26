ae85c7081cf52be543829e459a3f0f2091391aae
Prachi-215 <prachirakshak02@gmail.com>
2026-02-03 19:25:08 +0530

    Fix particle animation by adding missing duration property

--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -13,6 +13,7 @@ const Particles = () => {
         left: Math.random() * 100,
         top: Math.random() * 100,
         size: Math.random() * 10 + 6, 
+        duration: Math.random() * 10 + 15,
         delay: Math.random() * 5,
       }));
       setParticles(newParticles);
